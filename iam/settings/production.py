6f448b8ebb12af3cb6063cf2b2a56c9acd4b91a6
cranhurst <cranhurst@priest.com>
2018-07-12 12:39:00 +1200

    added variables for deployment

--- iam/settings/production.py
+++ iam/settings/production.py
@@ -18,6 +18,10 @@ EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
 
+    # django.db.backends.postgresql_psycopg2
+
+import dj_database_url
+
 DATABASES = {
     'default': dj_database_url.config(default=config('DATABASE_URL')
     )
