356d51f058b0888c5a892c32856cb4f89aa26884
cranhurst <cranhurst@priest.com>
2018-07-12 13:19:14 +1200

    added variables for deployment

--- iam/settings/production.py
+++ iam/settings/production.py
@@ -4,13 +4,6 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['thawing-ocean-69787.herokuapp.com', ]
 
-CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
-        'LOCATION': '127.0.0.1:11211',
-    }
-}
-
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.mailgun.org'
 EMAIL_PORT = 587
