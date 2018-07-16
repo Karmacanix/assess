c35e0aa6bfa8fddba8f3c29342ce9d2f29fb31a7
cranhurst <cranhurst@priest.com>
2018-07-16 14:56:22 +1200

    added more variables for deployment

--- iam/settings/base.py
+++ iam/settings/base.py
@@ -117,7 +117,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'staticfiles'),
+    os.path.join(PROJECT_ROOT, 'static', 'home'),
 )
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
